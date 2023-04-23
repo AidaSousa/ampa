93beeb55add7f6c096f77360fcfe8792ee4dac7f
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-04-23 20:24:11 +0200

    updated bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,10 +22,6 @@
     <header class="app-header"><a class="app-header__logo" href="index.html">Montevixia</a>
       <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <ul class="app-nav">
-        <li class="app-search">
-          <input class="app-search__input" type="search" placeholder="Buscador">
-          <button class="app-search__button"><i class="fa fa-search"></i></button>
-        </li>
         <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
 
